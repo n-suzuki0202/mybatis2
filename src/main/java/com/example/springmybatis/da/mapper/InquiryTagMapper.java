913a19b4cfd39738a2c3fda5f5f9eb6359cc4aca
n-suzuki0202 <nobuhiro.suzuki@intloop.com>
2022-08-19 17:05:37 +0900

    failed2...

--- src/main/java/com/example/springmybatis/da/mapper/InquiryTagMapper.java
+++ src/main/java/com/example/springmybatis/da/mapper/InquiryTagMapper.java
@@ -3,6 +3,7 @@ package com.example.springmybatis.da.mapper;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.session.RowBounds;
 import org.springframework.stereotype.Repository;
 
 import com.example.springmybatis.da.entity.InquiryTag;
@@ -12,6 +13,9 @@ import com.example.springmybatis.da.query.InquiryTagSelectQuery;
 @Mapper
 public interface InquiryTagMapper {
   InquiryTag find(int id);
-  List<InquiryTag> select(InquiryTagSelectQuery query);
+  List<InquiryTag> select(InquiryTagSelectQuery query, RowBounds rowBounds);
+  void insert(InquiryTag inquiryTag);
+  void update(InquiryTag inquiryTag);
+  void delete(int id);
   
 }
--- src/test/java/com/example/springmybatis/da/mapper/inquiryTagMapperTest.java
+++ src/test/java/com/example/springmybatis/da/mapper/inquiryTagMapperTest.java
@@ -3,11 +3,20 @@ package com.example.springmybatis.da.mapper;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.time.LocalDateTime;
+
+import org.apache.ibatis.session.RowBounds;
 import org.junit.jupiter.api.Test;
+
+import com.example.springmybatis.da.entity.InquiryTag;
+import com.example.springmybatis.da.query.InquiryTagSelectQuery;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static com.example.springmybatis.da.query.InquiryTagSelectQuery.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -21,4 +30,52 @@ class inquiryTagMapperTest {
     assertNotNull(inquiryTag);
     assertEquals("問合せ", inquiryTag.getDescription());
   }
+
+  @Test
+    void select() {
+        var query = new InquiryTagSelectQuery();
+
+        query.setDescription("問合せ");
+
+        query.addOrderCondition(OrderByField.INQUIRY_ID, SortOrder.DESC);
+        query.addOrderCondition(OrderByField.ID, SortOrder.ASC);
+
+        var list = inquiryTagMapper.select(query, new RowBounds(0, 100));
+
+        assertEquals(2, list.size());
+
+        var inquiryTag = list.get(0);
+
+        assertEquals(7, inquiryTag.getId());
+
+    }
+
+    @Test
+    void insertUpdateDelete() {
+        var inquiryTag = new InquiryTag();
+        inquiryTag.setInquiryId(1);
+        inquiryTag.setDescription("保存");
+        inquiryTag.setCreated(LocalDateTime.now());
+
+        inquiryTagMapper.insert(inquiryTag);
+
+        assertEquals(8, inquiryTag.getId());
+
+        inquiryTag.setDescription("保管");
+
+        inquiryTagMapper.update(inquiryTag);
+
+        var updatedInquiryTag = inquiryTagMapper.find(8);
+
+        assertNotNull(updatedInquiryTag);
+
+        assertEquals(updatedInquiryTag.getDescription(), inquiryTag.getDescription());
+
+        inquiryTagMapper.delete(8);
+
+        var deletedInquiryTag = inquiryTagMapper.find(8);
+
+        assertNull(deletedInquiryTag);
+
+    }
 }
